4f1b2741cd6b363bebc979d83bf556041edbee29
fmztsky <fmztsky@gmail.com>
2013-09-09 15:20:14 +0800

    ..

--- microCommune/index.js
+++
@@ -1,8 +0,0 @@
-var server = require("./server/server.js");
-var router = require("./server/router.js");
-var requestHandle = require("./server/handle.js");
-
-var handle = {};
-handle["/sendMsg"] = requestHandle.server2;
-
-server.start(router.route,handle);
