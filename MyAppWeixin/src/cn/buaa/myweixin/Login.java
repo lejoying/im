9e64f2f17141230e4bf81e54fb249e3c22114676
fmztsky <fmztsky@gmail.com>
2013-10-09 11:52:47 +0800

    .

--- MyAppWeixin/src/cn/buaa/myweixin/Login.java
+++ MyAppWeixin/src/cn/buaa/myweixin/Login.java
@@ -23,37 +23,7 @@ public class Login extends Activity {
     }
 
     public void login_mainweixin(View v) {
-    	if("buaa".equals(mUser.getText().toString()) && "123".equals(mPassword.getText().toString()))   //判断 帐号和密码
-        {
-             Intent intent = new Intent();
-             intent.setClass(Login.this,LoadingActivity.class);
-             startActivity(intent);
-             //Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_SHORT).show();
-          }
-        else if("".equals(mUser.getText().toString()) || "".equals(mPassword.getText().toString()))   //判断 帐号和密码
-        {
-        	new AlertDialog.Builder(Login.this)
-			.setIcon(getResources().getDrawable(R.drawable.login_error_icon))
-			.setTitle("登录错误")
-			.setMessage("微信帐号或者密码不能为空，\n请输入后再登录！")
-			.create().show();
-         }
-        else{
-           
-        	new AlertDialog.Builder(Login.this)
-			.setIcon(getResources().getDrawable(R.drawable.login_error_icon))
-			.setTitle("登录失败")
-			.setMessage("微信帐号或者密码不正确，\n请检查后重新输入！")
-			.create().show();
-        }
     	
-    	//登录按钮
-    	/*
-      	Intent intent = new Intent();
-		intent.setClass(Login.this,Whatsnew.class);
-		startActivity(intent);
-		Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_SHORT).show();
-		this.finish();*/
       }  
     public void login_back(View v) {     //标题栏 返回按钮
       	this.finish();
