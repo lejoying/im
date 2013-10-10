c9ad92528d4e65bdec2e9d451bbd4b2e4f084307
fmztsky <fmztsky@gmail.com>
2013-10-10 16:52:11 +0800

    imageserver

---
+++ microcommune/server/imageserver/main.js
@@ -0,0 +1,28 @@
+/**
+ * The simplest framework for image server.
+ * Run "$ sudo node main.js" to start the server.
+ */
+
+var http = require("http");
+var route = require("./lib/route");
+var routemap = require("./routemap");
+
+var i = 1;
+
+http.createServer(
+    function (request, response) {
+
+        response.writeHead(200, {
+            "Content-Type":"application/json; charset=UTF-8"
+        });
+        route(routemap, request.url, request, response);
+
+        i++;
+        console.log("The image server has been accessed " + i);
+        if (response.asynchronous == null) {
+            response.end();
+        }
+
+    }).listen(8072);
+
+console.log("The image server is running.");
