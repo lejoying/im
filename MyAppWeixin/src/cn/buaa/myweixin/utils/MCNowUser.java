833a07498ca8a9f4bff6b4ef27f62341035024c5
fmztsky <fmztsky@gmail.com>
2013-10-15 20:09:32 +0800

    ..

---
+++ MyAppWeixin/src/cn/buaa/myweixin/utils/MCNowUser.java
@@ -0,0 +1,23 @@
+package cn.buaa.myweixin.utils;
+
+public class MCNowUser {
+	private static Account account;
+
+	public static void setNowUser(String phone, String head, String nickName,
+			String mainBusiness,String status) {
+		MCNowUser.account = new Account();
+		MCNowUser.account.setStatus(status);
+		MCNowUser.account.setPhone(phone);
+		MCNowUser.account.setNickName(nickName);
+		MCNowUser.account.setMainBusiness(mainBusiness);
+		MCNowUser.account.setHead(head);
+	}
+
+	public static void setNowUser(Account account){
+		MCNowUser.account = account;
+	}
+	
+	public static Account getNowUser() {
+		return account;
+	}
+}
