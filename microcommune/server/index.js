e6602d9064b3bae6ac678ce16e503b1f93bd3d06
coolspan <coolspan@sina.cn>
2013-11-21 14:20:40 +0800

    update

--- microcommune/server/index.js
+++ microcommune/server/index.js
@@ -1,4 +1,4 @@
 var serverSetting = require('./settings.js');
 var pushserver = require("./mcserver/main.js");
 var imageserver = require("./imageserver/index.js");
-var imageserver = require("./alipayserver/main.js");
+var alipayserver = require("./alipayserver/main.js");
