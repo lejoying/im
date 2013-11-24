01ddd5d6d49cf400baff7b5570a854527fcdf5fa
coolspan <coolspan@sina.cn>
2013-11-24 17:37:57 +0800

    ...

--- microcommune/server/alipayserver/main.js
+++ microcommune/server/alipayserver/main.js
@@ -1,5 +1,5 @@
 var express = require('express')
-    , routes = require('./index.js');
+    , routes = require('./alipayManage');
 var express = require("express");
 var app = express();
 //var app = module.exports;
