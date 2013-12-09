2d913fea6a94ad0bd5ca4881d94edfa5eb6cbda6
fmztsky <fmztsky@gmail.com>
2013-12-09 23:59:12 +0800

    SMS

--- microcommune/server/index.js
+++ microcommune/server/index.js
@@ -3,3 +3,4 @@ var pushserver = require("./mcserver/main.js");
 var imageserver = require("./imageserver/main.js");
 var voiceserver = require("./voiceserver/main.js");
 var alipayserver = require("./alipayserver/main.js");
+var smsserver = require("./smsserver/main.js");
