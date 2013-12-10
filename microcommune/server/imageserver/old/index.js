a8f219801c99035fa44e97a2cd30b1aedd530a9d
coolspan <coolspan@sina.cn>
2013-12-10 18:52:48 +0800

    ....delete old

--- microcommune/server/imageserver/old/index.js
+++
@@ -1,7 +0,0 @@
-var server = require("./server");
-var route  = require("./route");
-
-//var serverSetting = root.globaldata.serverSetting;
-//console.log("index: " + JSON.stringify(serverSetting));
-
-server.start(route.route);
