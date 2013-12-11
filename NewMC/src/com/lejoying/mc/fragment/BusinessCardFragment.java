dd4f74ac23f72fd8215d14c35afca2d8ed0206b4
fmztsky <fmztsky@gmail.com>
2013-12-11 19:09:48 +0800

    ..

---
+++ NewMC/src/com/lejoying/mc/fragment/BusinessCardFragment.java
@@ -0,0 +1,13 @@
+package com.lejoying.mc.fragment;
+
+import android.widget.EditText;
+
+public class BusinessCardFragment extends BaseListFragment {
+
+	@Override
+	protected EditText showSoftInputOnShow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
