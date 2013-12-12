552b4c0f3379080f5845c300ea9efd29cb350e27
fmztsky <fmztsky@gmail.com>
2013-12-12 19:14:47 +0800

    ..

---
+++ NewMC/src/com/lejoying/mc/fragment/FriendEditTabFragment.java
@@ -0,0 +1,13 @@
+package com.lejoying.mc.fragment;
+
+import android.widget.EditText;
+
+public class FriendEditTabFragment extends BaseFragment {
+
+	@Override
+	protected EditText showSoftInputOnShow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
