61a9e878fa45638f9dfd17c4684771f5b5b7a1f7
coolspan <coolspan@sina.cn>
2014-01-11 18:19:51 +0800

    web Jrop AccountHeadImg update

--- microcommune/webmc/static/js/basecontrol.js
+++ microcommune/webmc/static/js/basecontrol.js
@@ -1,9 +1,65 @@
 var selectPanel = "js_tempChatTop";
 var scrollFlagTempChatTop = false;
 var image = new Image();
+var vData;
+var loadImageFlag = false;
 $(function () {//show js_modify_jcrophead_show
-    image.src = "/static/images/face_man.png";
+    var img = new Image();
+    /*$.ajax({
+     type: "GET",
+     url: "/image/get?",
+     data: {
+     phone: '121',
+     accessKey: "lejoying",
+     filename: "2a90e39e7a8d7b380fcec6cffd59172729b3cf49.png"
+     },
+     success: function (data) {
+     alert(data["提示信息"]);
+     image.src = data.image;
+     }
+     });*/
+//    image.src = "http://im.lejoying.com/static/images/face_man.png";
+    Image.Source = "http://im.lejoying.com/static/images/face_man.png";
+//    img.crossOrigin = "*";
+//    image.src = "/static/images/face_man.png";
+//    img.crossOrigin = "Anonymous";
 
+    img.onload = function () {
+        console.log(img.complete);
+        console.log("img:" + img.height + "-" + img.width + "--");
+        var temp_ctx, temp_canvas;
+        temp_canvas = document.createElement('canvas');
+        temp_canvas.style.marginTop = "-1800px";
+        temp_ctx = temp_canvas.getContext('2d');
+        temp_canvas.width = img.width;
+        temp_canvas.height = img.height;
+        temp_ctx.drawImage(img, 0, 0);
+        document.body.appendChild(temp_canvas);
+        console.log(temp_canvas);
+        //        var data = (temp_canvas.toDataURL("image/png"));
+        var data = (temp_canvas.toDataURL("image/png"));
+        //        image.src = data;
+        //    $(images).attr("src", temp_canvas.toDataURL());
+        console.log(data);
+        //        $("#js_modify_head_file").attr("src", data);
+        //        console.log(image.width + "--" + image.height);
+    }
+
+    /*    image.src = "/static/images/face_woman.png";
+     var images = document.getElementById("js_modify_head_file");
+     var temp_ctx, temp_canvas;
+     temp_canvas = document.createElement('canvas');
+     temp_ctx = temp_canvas.getContext('2d');
+     temp_canvas.width = images.width
+     temp_canvas.height = images.height;
+     temp_ctx.drawImage(images, 0, 0, images.width, images.height, 0, 0, images.width, images.height);
+     var data = (temp_canvas.toDataURL("image/png"));
+     image.src = data;
+     //    $(images).attr("src", temp_canvas.toDataURL());
+     console.log(data);
+     $("#js_modify_head_file").attr("src", data);
+     console.log(image.width + "--" + image.height);*/
+//    image = document.getElementById("js_modify_head_file");
     $(".js_modifyAccountHeadImgPanel").hide();
     $(".scrollDiv1").hide();
     $(".js_tempChatTop").addClass("conmuButton_chatStyle");
@@ -63,12 +119,6 @@ $(function () {//show js_modify_jcrophead_show
     });
 
     $(".js_Account_HeadImg").click(function () {
-        $("#js_modify_head_file").attr("src", "/static/images/face_man.png");
-        $("#js_modify_jcrophead_show").attr("src", "/static/images/face_man.png");
-        /*$("#js_modify_head_file").css({
-         "width": "234px",
-         "height": "234px"
-         });*/
         if ($(".js_modifyAccountHeadImgPanel").css('display') != "block") {
             $(".js_modifyAccountHeadImgPanel").css({
                 "top": "40px",
@@ -110,25 +160,30 @@ $(function () {//show js_modify_jcrophead_show
                 $(".js_modifyAccountHeadImgPanel").show();
             }
         }
-        $("#js_modify_head_file").Jcrop();
-        /*$("#js_modify_head_file").Jcrop({
-         onChange: showPreview,
-         onSelect: showPreview,
-         aspectRatio: 1,
-         setSelect: [0, 0, 100, 100]
-         });*/
+        $("#js_head_file").Jcrop({
+            onChange: showPreview,
+            onSelect: showPreview,
+            aspectRatio: 1,
+            setSelect: [0, 0, 100, 100]
+        });
+        $(".jcrop-holder").css({
+            "top": "80px",
+            "left": "40px"
+        });
+        $(".jcrop-keymgr").css({
+            "visibility": "hidden"
+        });
     });
     $(".js_modify_head_close").click(function () {
         $(".js_modifyAccountHeadImgPanel").hide();
     });
-
-//    new Drag($("#js_modifyAccountHeadImgPanel")[0]);
+    new Drag($("#js_modifyAccountHeadImgPanel")[0]);
 });
-//简单的事件处理程序，响应自onChange,onSelect事件，按照上面的Jcrop调用
 function showPreview(coords) {
+    console.log(image.src.length);
     if (parseInt(coords.w) > 0) {
-        var cx = image.width / 250;
-        var cy = image.height / 250;
+        var cx = image.width / 234;
+        var cy = image.height / 234;
         var cwidth = Math.round(coords.w * cx);
         var cheight = Math.round(coords.h * cy);
         var ax = coords.x * cx;
@@ -139,10 +194,16 @@ function showPreview(coords) {
         temp_canvas.width = 100
         temp_canvas.height = 100;
         temp_ctx.drawImage(image, ax, ay, cwidth, cheight, 0, 0, 100, 100);
-        vData = temp_canvas.toDataURL();
-        tempData = temp_canvas.toDataURL();
-        $("#js_modify_jcrophead_show").attr('src', vData);
+        var vData = temp_canvas.toDataURL();
+        console.log("vData:" + vData + "---length:" + vData.length + "cwidth:" + cwidth + "--cheight:" +
+            cheight + "--ax:" + ax + "--ay:" + ay + "--canves:" + temp_canvas.height);
+//        tempData = temp_canvas.toDataURL();
+        setImagesPostion(vData);
     }
+}
+function setImagesPostion(data) {
+    $("#js_modify_jcrophead_show").attr('src', data);
+    $(".js_accountHead").attr('src', data);
 }
 function Drag(o) {
     var rDrag = {
