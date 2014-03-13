0a9f35f864fb03442be6da0a78687f68df51d7d1
fmztsky <fmztsky@gmail.com>
2014-03-13 19:11:49 +0800

    optimize

--- AutoSendSMS/src/com/lejoying/autosendsms/MainActivity.java
+++ AutoSendSMS/src/com/lejoying/autosendsms/MainActivity.java
@@ -24,12 +24,20 @@ public class MainActivity extends Activity implements OnClickListener {
 	TextView tv_list;
 	TextView tv_send;
 
+	StatusReceiver mReceiver;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initView();
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(SMSService.ACTION);
+		mReceiver = new StatusReceiver();
+		registerReceiver(mReceiver, intentFilter);
 
 		startService(new Intent(this, SMSService.class));
 
@@ -62,22 +70,99 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btn_start:
+			Intent serviceStart = new Intent(this, SMSService.class);
+			serviceStart.putExtra("operation", "start");
+			startService(serviceStart);
 			start();
 			break;
 		case R.id.btn_stop:
+			Intent serviceStop = new Intent(this, SMSService.class);
+			serviceStop.putExtra("operation", "stop");
+			startService(serviceStop);
 			stop();
 			break;
 		case R.id.btn_startsend:
+			Intent serviceStartSend = new Intent(this, SMSService.class);
+			serviceStartSend.putExtra("operation", "startSend");
+			startService(serviceStartSend);
 			startSend();
 			break;
 		case R.id.btn_stopsend:
+			Intent serviceStopSend = new Intent(this, SMSService.class);
+			serviceStopSend.putExtra("operation", "stopSend");
+			startService(serviceStopSend);
 			stopSend();
 			break;
 		case R.id.btn_clearqueue:
-			clearQueue();
+			Intent serviceClear = new Intent(this, SMSService.class);
+			serviceClear.putExtra("operation", "clear");
+			startService(serviceClear);
 			break;
 		default:
 			break;
 		}
 	}
+
+	public void start() {
+		btn_start.setClickable(false);
+		btn_start.setTextColor(Color.GRAY);
+		btn_stop.setClickable(true);
+		btn_stop.setTextColor(Color.BLACK);
+	}
+
+	public void stop() {
+		btn_start.setClickable(true);
+		btn_start.setTextColor(Color.BLACK);
+		btn_stop.setClickable(false);
+		btn_stop.setTextColor(Color.GRAY);
+	}
+
+	public void startSend() {
+		btn_startSend.setClickable(false);
+		btn_startSend.setTextColor(Color.GRAY);
+		btn_stopSend.setClickable(true);
+		btn_stopSend.setTextColor(Color.BLACK);
+	}
+
+	public void stopSend() {
+		btn_startSend.setClickable(true);
+		btn_startSend.setTextColor(Color.BLACK);
+		btn_stopSend.setClickable(false);
+		btn_stopSend.setTextColor(Color.GRAY);
+	}
+
+	class StatusReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			parseStatus(intent);
+		}
+
+		void parseStatus(Intent intent) {
+			if (intent.getBooleanExtra("isConnection", false)) {
+				start();
+			} else {
+				stop();
+			}
+			if (intent.getBooleanExtra("isStartSend", false)) {
+				startSend();
+			} else {
+				stopSend();
+			}
+
+			String nowSending = intent.getStringExtra("nowSending");
+
+			if (nowSending != null) {
+				tv_nowsending.setText("正在发送到：" + nowSending);
+			}
+
+			int queueCount = intent.getIntExtra("queueSize", 0);
+			tv_list.setText("队列中条数：" + String.valueOf(queueCount));
+
+			int successCount = intent.getIntExtra("successCount", 0);
+			tv_send.setText("已发送条数：" + String.valueOf(successCount));
+
+		}
+	}
+
 }
--- AutoSendSMS/src/com/lejoying/autosendsms/SMSService.java
+++ AutoSendSMS/src/com/lejoying/autosendsms/SMSService.java
@@ -4,11 +4,12 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
-import com.lejoying.utils.NetworkHandler;
-import com.lejoying.utils.NetworkHandler.NetConnection;
-import com.lejoying.utils.NetworkHandler.Settings;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.PendingIntent;
@@ -21,7 +22,16 @@ import android.net.ConnectivityManager;
 import android.os.IBinder;
 import android.telephony.SmsManager;
 
+import com.lejoying.autosendsms.handler.NetworkHandler;
+import com.lejoying.autosendsms.handler.NetworkHandler.NetConnection;
+import com.lejoying.autosendsms.handler.NetworkHandler.Response;
+import com.lejoying.autosendsms.handler.NetworkHandler.ResponseHandler;
+import com.lejoying.autosendsms.handler.NetworkHandler.Settings;
+import com.lejoying.autosendsms.handler.StreamParser;
+
 public class SMSService extends Service {
+
+	public static final String ACTION = "com.lejoying.autosendsms.smsstatuschanged";
 
 	SmsManager smsManager;
 	PendingIntent sentPI;
@@ -30,7 +40,12 @@ public class SMSService extends Service {
 	BroadcastReceiver deliveredReceiver;
 
 	NetworkHandler networkHandler;
+	ResponseHandler responseHandler;
+	SMSSender smsSender;
+
 	NetConnection netConnection;
+
+	Status status;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -40,28 +55,17 @@ public class SMSService extends Service {
 
 	@Override
 	public void onCreate() {
+		status = new Status();
 
 		networkHandler = new NetworkHandler(1);
-		netConnection = new NetConnection() {
+		responseHandler = new ResponseHandler(1);
+		smsSender = new SMSSender();
 
-			@Override
-			protected void success(InputStream is,
-					HttpURLConnection httpURLConnection) {
-				// TODO Auto-generated method stub
+		status.isStartSend = smsSender.isStartSend();
 
-			}
-
-			@Override
-			protected void settings(Settings settings) {
-				settings.url = "http://115.28.51.197:8074/api2/sms/event";
-				settings.timeout = 30000;
-				String sessionID = String.valueOf(new Date().getTime());
-				Map<String, String> params = new HashMap<String, String>();
-				params.put("sessionID", sessionID);
-				settings.params = params;
-				settings.circulating = true;
-			}
-		};
+		resetConnection();
+		networkHandler.connection(netConnection);
+		smsSender.startSend();
 
 		smsManager = SmsManager.getDefault();
 		String SENT_SMS_ACTION = "SENT_SMS_ACTION";
@@ -78,6 +82,9 @@ public class SMSService extends Service {
 				switch (getResultCode()) {
 				case Activity.RESULT_OK:
 					// System.out.println("RESULT_OK");
+					status.successCount++;
+					status.nowSending = "wait...";
+					sendStatusChanged();
 					break;
 				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
 					// System.out.println("RESULT_ERROR_GENERIC_FAILURE");
@@ -110,10 +117,211 @@ public class SMSService extends Service {
 		super.onCreate();
 	}
 
-	public void sendSMS(String phone, String text) {
-		if (phone != null && !phone.equals("") && text != null
-				&& !text.equals("")) {
-			smsManager.sendTextMessage(phone, null, text, sentPI, deliverPI);
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		if (intent != null) {
+			String operation = intent.getStringExtra("operation");
+			if (operation != null && !operation.equals("")) {
+				if (operation.equals("start")) {
+					if (netConnection == null || netConnection.isDisconnected()) {
+						resetConnection();
+						networkHandler.connection(netConnection);
+					}
+					status.isConnection = true;
+					sendStatusChanged();
+				} else if (operation.equals("stop")) {
+					if (netConnection != null) {
+						netConnection.disConnection();
+						netConnection = null;
+					}
+					status.isConnection = false;
+					sendStatusChanged();
+				} else if (operation.equals("startSend")) {
+					smsSender.startSend();
+					status.isStartSend = true;
+					sendStatusChanged();
+				} else if (operation.equals("stopSend")) {
+					smsSender.stopSend();
+					status.isStartSend = true;
+					sendStatusChanged();
+				} else if (operation.equals("clear")) {
+					smsSender.clear();
+				}
+			}
+		}
+		status.isConnection = netConnection != null ? !netConnection
+				.isDisconnected() : false;
+		status.isStartSend = smsSender.isStartSend();
+		sendStatusChanged();
+		return super.onStartCommand(intent, START_STICKY, startId);
+	}
+
+	void resetConnection() {
+		netConnection = new NetConnection() {
+
+			@Override
+			protected void failed(int failedType, int responseCode) {
+				switch (failedType) {
+				case FAILED_TIMEOUT:
+
+					break;
+
+				default:
+					synchronized (this) {
+						try {
+							wait(5000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					break;
+				}
+			}
+
+			@Override
+			protected void success(InputStream is,
+					final HttpURLConnection httpURLConnection) {
+				responseHandler.exclude(new Response(is) {
+					@Override
+					public void handleResponse(InputStream is) {
+						JSONObject jObject = StreamParser.parseToJSONObject(is);
+						httpURLConnection.disconnect();
+						try {
+							String phone = jObject.getString("phone");
+							String message = jObject.getString("message");
+							smsSender.sendSMS(phone, message);
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+
+			@Override
+			protected void settings(Settings settings) {
+				settings.url = "http://115.28.51.197:8074/api2/sms/event";
+				settings.timeout = 30000;
+				String sessionID = String.valueOf(new Date().getTime());
+				Map<String, String> params = new HashMap<String, String>();
+				params.put("sessionID", sessionID);
+				settings.params = params;
+				settings.circulating = true;
+			}
+		};
+	}
+
+	public void sendStatusChanged() {
+		Intent broadcast = new Intent(ACTION);
+		broadcast.putExtra("isConnection", status.isConnection);
+		broadcast.putExtra("isStartSend", status.isStartSend);
+		broadcast.putExtra("queueSize", status.queueSize);
+		broadcast.putExtra("nowSending", status.nowSending);
+		broadcast.putExtra("successCount", status.successCount);
+		sendBroadcast(broadcast);
+	}
+
+	public static class Status {
+		boolean isConnection;
+		boolean isStartSend;
+		int queueSize;
+		String nowSending = "wait...";
+		int successCount;
+	}
+
+	public class SMSSender {
+
+		public Queue<SMS> mSMSQueue;
+
+		boolean startSend;
+
+		WorkThread workThread;
+
+		public SMSSender() {
+			mSMSQueue = new LinkedList<SMSService.SMSSender.SMS>();
+			workThread = new WorkThread();
+			workThread.start();
+			status.isStartSend = startSend;
+		}
+
+		public synchronized void stopSend() {
+			startSend = false;
+			status.isStartSend = startSend;
+			notify();
+		}
+
+		public void startSend() {
+			startSend = true;
+			status.isStartSend = startSend;
+		}
+
+		public boolean isStartSend() {
+			return startSend;
+		}
+
+		public synchronized void clear() {
+			mSMSQueue.clear();
+			status.queueSize = mSMSQueue.size();
+			sendStatusChanged();
+		}
+
+		public synchronized void sendSMS(String phone, String message) {
+			mSMSQueue.offer(new SMS(phone, message));
+			status.queueSize = mSMSQueue.size();
+			sendStatusChanged();
+			if (startSend) {
+				notify();
+			}
+		}
+
+		synchronized SMS getSMS() throws InterruptedException {
+			if (!startSend || mSMSQueue.size() == 0) {
+				wait();
+			}
+			return mSMSQueue.poll();
+		}
+
+		class SMS {
+			String phone;
+			String text;
+
+			public SMS() {
+				// TODO Auto-generated constructor stub
+			}
+
+			public SMS(String phone, String text) {
+				this.phone = phone;
+				this.text = text;
+			}
+
+		}
+
+		class WorkThread extends Thread {
+			boolean interrupt;
+
+			@Override
+			public void run() {
+				while (!interrupt) {
+					SMS sms = null;
+					try {
+						while ((sms = getSMS()) == null)
+							;
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					if (sms.phone != null && !sms.phone.equals("")
+							&& sms.text != null && !sms.text.equals("")) {
+						status.queueSize = mSMSQueue.size();
+						status.nowSending = sms.phone;
+						sendStatusChanged();
+						smsManager.sendTextMessage(sms.phone, null, sms.text,
+								sentPI, deliverPI);
+					}
+				}
+			}
 		}
 	}
 }
