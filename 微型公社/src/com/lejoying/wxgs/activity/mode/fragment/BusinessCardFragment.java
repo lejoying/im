fb4e1b2a316256676cca49a815408b6c7cc90018
wjjm555 <139040491@qq.com>
2014-04-23 08:23:24 +0800

    add ReleaseActivity

--- 微型公社/src/com/lejoying/wxgs/activity/mode/fragment/BusinessCardFragment.java
+++ 微型公社/src/com/lejoying/wxgs/activity/mode/fragment/BusinessCardFragment.java
@@ -12,10 +12,6 @@ import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
@@ -27,7 +23,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
 import android.util.DisplayMetrics;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
