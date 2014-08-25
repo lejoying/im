9d363e888546b1efa1404c308b2110bce886bcdf
wjjm555 <139040491@qq.com>
2014-08-25 10:36:56 +0800

    To optimize the ChatActivity

--- androidApp/src/com/open/welinks/controller/FriendsSubController.java
+++ androidApp/src/com/open/welinks/controller/FriendsSubController.java
@@ -1,17 +1,31 @@
 package com.open.welinks.controller;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+
 import com.open.welinks.model.Data;
 import com.open.welinks.view.FriendsSubView;
 
 public class FriendsSubController {
+
+	public String tag = "UserIntimateController";
 
 	public Data data = Data.getInstance();
 
 	public FriendsSubView thisView;
 	public FriendsSubController thisController;
 
-	public void initializeListeners() {
+	public OnClickListener mOnClickListener;
 
+	public void initializeListeners() {
+		mOnClickListener = new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				// TODO Auto-generated method stub
+
+			}
+		};
 	}
 
 	public void bindEvent() {
--- androidApp/src/com/open/welinks/controller/MessagesSubController.java
+++ androidApp/src/com/open/welinks/controller/MessagesSubController.java
@@ -1,5 +1,8 @@
 package com.open.welinks.controller;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+
 import com.open.welinks.model.Data;
 import com.open.welinks.view.MessagesSubView;
 
@@ -8,9 +11,21 @@ public class MessagesSubController {
 	public Data data = Data.getInstance();
 	public String tag = "UserIntimateController";
 	public MessagesSubView thisView;
+	public MessagesSubController thisconController;
+
+	public OnClickListener mOnClickListener;
+
 	public void initializeListeners() {
-		
+		mOnClickListener = new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				// TODO Auto-generated method stub
+
+			}
+		};
 	}
+
 	public void bindEvent() {
 	}
 
