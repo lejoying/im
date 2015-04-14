45d42d6fd76fcb26636e9cb42cfcb561a18814c5
coolspan <coolspan@sina.cn>
2015-04-14 17:00:54 +0800

    add default locations.

--- androidApp/src/com/open/welinks/view/SquareSubView.java
+++ androidApp/src/com/open/welinks/view/SquareSubView.java
@@ -1,13 +1,12 @@
 package com.open.welinks.view;
 
-import java.io.File;
 import java.util.ArrayList;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.FrameLayout.LayoutParams;
@@ -17,8 +16,6 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 import com.open.lib.MyLog;
 import com.open.lib.TouchImageView;
 import com.open.lib.TouchTextView;
@@ -29,7 +26,6 @@ import com.open.welinks.R;
 import com.open.welinks.controller.SquareSubController;
 import com.open.welinks.model.API;
 import com.open.welinks.model.Data;
-import com.open.welinks.model.Data.Relationship.Group;
 import com.open.welinks.model.Parser;
 import com.open.welinks.model.TaskManageHolder;
 import com.open.welinks.oss.DownloadFile;
@@ -93,37 +89,47 @@ public class SquareSubView {
 		ArrayList<Integer> iconList = new ArrayList<Integer>() {
 			{
 				add(R.drawable.sidebar_icon_discover_normal);
-				add(R.drawable.sidebar_icon_days_normal);
+				add(R.drawable.sidebar_icon_beauty_normal);
 				add(R.drawable.sidebar_icon_group_normal);
 				add(R.drawable.sidebar_icon_category_normal);
 				add(R.drawable.sidebar_icon_test_normal);
-				add(R.drawable.sidebar_icon_beauty_normal);
+				add(R.drawable.sidebar_icon_days_normal);
 			}
 		};
 		@SuppressWarnings("serial")
 		ArrayList<String> list = new ArrayList<String>() {
 			{
-				add("群组分享");
-				add("热门话题");
+				add("我的群组");
+				add("我的帖子");
 				add("附近的群");
 				add("分类推荐");
 				add("附近的人");
-				add("与我相关");
+				add("附近商家");
 			}
 		};
+		float width = (this.displayMetrics.widthPixels - 30 * this.displayMetrics.density) / 2;
+		float height = (width - 5) / 2;
 		this.locationListBody.height = 0 * this.displayMetrics.density;
 		selectBody = new SelectBody(locationListBody);
 		selectBody.initialize(0);
-		RelativeLayout.LayoutParams layoutParams0 = new RelativeLayout.LayoutParams(android.widget.RelativeLayout.LayoutParams.MATCH_PARENT, (int) (190 * this.displayMetrics.density));
+		float w0 = width * 2 + 10 * this.displayMetrics.density, h0 = height * 2;
+		FrameLayout.LayoutParams params0 = (LayoutParams) selectBody.iconView.getLayoutParams();
+		params0.height = (int) h0 / 4;
+		params0.width = (int) params0.height;// params0.
+		// selectBody.iconView.setX(x);
+		selectBody.setContent(iconList.get(0), "中关村生活圈");
+		selectBody.iconView.setImageResource(R.drawable.ic_main_more);
+		selectBody.iconView.setAlpha(0.875f);
+		selectBody.nameView.setGravity(Gravity.CENTER);
+		RelativeLayout.LayoutParams layoutParams0 = new RelativeLayout.LayoutParams((int) w0, (int) h0);
 		this.locationListBody.containerView.addView(selectBody.cardView, layoutParams0);
-		this.locationListBody.height += 190 * this.displayMetrics.density;
+		this.locationListBody.height += h0 + 10 * this.displayMetrics.density;
 		selectBody.y = 0 * displayMetrics.density;
-		selectBody.cardView.setY(selectBody.y);
+		selectBody.cardView.setX(10 * displayMetrics.density);
+		selectBody.cardView.setY(10 * this.displayMetrics.density);
 
-		float width = (this.displayMetrics.widthPixels - 30 * this.displayMetrics.density) / 2;
+		this.locationListBody.height += 10 * this.displayMetrics.density;
 
-		float height = (width - 5) / 2;
-		this.locationListBody.height += 15 * this.displayMetrics.density;
 		for (int i = 0; i < list.size(); i++) {
 			String name = list.get(i);
 			SelectBody body = new SelectBody(locationListBody);
@@ -142,7 +148,7 @@ public class SquareSubView {
 				h = height;
 				body.x = this.displayMetrics.widthPixels / 2 + 5 * this.displayMetrics.density;
 				body.y = this.locationListBody.height;
-				body.cardView.setBackgroundResource(R.drawable.button_7174e0_background);
+				body.cardView.setBackgroundResource(R.drawable.button_39aadf_background);
 			} else if (i == 2) {
 				w = width;
 				h = height;
@@ -166,7 +172,7 @@ public class SquareSubView {
 				h = height;
 				body.x = this.displayMetrics.widthPixels / 2 + 5 * this.displayMetrics.density;
 				body.y = this.locationListBody.height + height * 3 + 20 * this.displayMetrics.density;
-				body.cardView.setBackgroundResource(R.drawable.button_39aadf_background);
+				body.cardView.setBackgroundResource(R.drawable.button_7174e0_background);// button_7174e0_background
 			}
 			body.cardView.setX(body.x);
 			body.cardView.setY(body.y);
@@ -188,7 +194,7 @@ public class SquareSubView {
 		public TouchImageView iconView;
 		public TouchTextView nameView;
 
-		public TouchImageView coverView;
+		public TouchTextView coverView;
 
 		public SelectBody(ListBody1 listBody) {
 			listBody.super();
@@ -203,9 +209,15 @@ public class SquareSubView {
 				this.cardView.setOnTouchListener(thisController.mOnTouchListener);
 				this.cardView.setOnClickListener(thisController.mOnClickListener);
 			} else if (type == 0) {
-				this.cardView = (TouchView) mInflater.inflate(R.layout.view_location_imageview, null);
-				this.coverView = (TouchImageView) this.cardView.findViewById(R.id.cover);
-				setConver();
+				this.cardView = (TouchView) mInflater.inflate(R.layout.view_location_item, null);
+				this.iconView = (TouchImageView) this.cardView.findViewById(R.id.icon);
+				this.nameView = (TouchTextView) this.cardView.findViewById(R.id.name);
+
+				// this.cardView = (TouchView) mInflater.inflate(R.layout.view_location_imageview, null);
+				// this.coverView = (TouchTextView) this.cardView.findViewById(R.id.cover);
+				// this.coverView.setText("群组分享");
+				// this.coverView.setBackgroundResource(R.drawable.button_de485e_background);
+				// setConver();
 			}
 			return this.cardView;
 		}
@@ -220,41 +232,41 @@ public class SquareSubView {
 	}
 
 	public void setConver() {
-		if (data.relationship == null || data.relationship.groupsMap == null || data.localStatus == null || data.localStatus.localData == null) {
-			log.e("square sub view setCover null");
-			return;
-		}
-		final Group group = data.relationship.groupsMap.get(data.localStatus.localData.currentSelectedGroup);
-		if (group != null) {
-			File file = new File(taskManageHolder.fileHandler.sdcardBackImageFolder, group.cover);
-			if (group.cover == null || "".equals(group.cover)) {
-				taskManageHolder.imageLoader.displayImage("drawable://" + R.drawable.tempicon, selectBody.coverView);
-				return;
-			}
-			final String path = file.getAbsolutePath();
-			if (file.exists()) {
-				taskManageHolder.imageLoader.displayImage("file://" + path, selectBody.coverView, new SimpleImageLoadingListener() {
-					@Override
-					public void onLoadingStarted(String imageUri, View view) {
-					}
-
-					@Override
-					public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-						downloadConver(selectBody.coverView, group.cover, path);
-					}
-
-					@Override
-					public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-					}
-				});
-			} else {
-				if (group.cover != null) {
-					downloadConver(selectBody.coverView, group.cover, path);
-				} else {
-					taskManageHolder.imageLoader.displayImage("drawable://" + R.drawable.tempicon, selectBody.coverView);
-				}
-			}
-		}
+		// if (data.relationship == null || data.relationship.groupsMap == null || data.localStatus == null || data.localStatus.localData == null) {
+		// log.e("square sub view setCover null");
+		// return;
+		// }
+		// final Group group = data.relationship.groupsMap.get(data.localStatus.localData.currentSelectedGroup);
+		// if (group != null) {
+		// File file = new File(taskManageHolder.fileHandler.sdcardBackImageFolder, group.cover);
+		// if (group.cover == null || "".equals(group.cover)) {
+		// taskManageHolder.imageLoader.displayImage("drawable://" + R.drawable.tempicon, selectBody.coverView);
+		// return;
+		// }
+		// final String path = file.getAbsolutePath();
+		// if (file.exists()) {
+		// taskManageHolder.imageLoader.displayImage("file://" + path, selectBody.coverView, new SimpleImageLoadingListener() {
+		// @Override
+		// public void onLoadingStarted(String imageUri, View view) {
+		// }
+		//
+		// @Override
+		// public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+		// downloadConver(selectBody.coverView, group.cover, path);
+		// }
+		//
+		// @Override
+		// public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+		// }
+		// });
+		// } else {
+		// if (group.cover != null) {
+		// downloadConver(selectBody.coverView, group.cover, path);
+		// } else {
+		// taskManageHolder.imageLoader.displayImage("drawable://" + R.drawable.tempicon, selectBody.coverView);
+		// }
+		// }
+		// }
 	}
 
 	public void downloadConver(ImageView coverView, String converName, String path) {
